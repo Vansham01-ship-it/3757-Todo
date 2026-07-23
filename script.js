4811977edebb3aa19dd1a9cf59566faf371812c3
Vansham01-ship-it <vanshamb8837@gmail.com>
2026-07-23 22:02:52 +0530

    Vansham

--- script.js
+++ script.js
@@ -5,26 +5,69 @@ const list = document.getElementById("ul")
 let todo = []
 
 
+function loadTodo(){
+    const savedTodo = localStorage.getItem("todo")
+    if(savedTodo){
+        todo = JSON.parse(savedTodo)
+    }
+    renderTodo()
+}
+
+// Save tasks to localStorage
+function saveTodo(){
+    localStorage.setItem("todo", JSON.stringify(todo))
+}
+
 function addTodo(){
-    const task = inputBox.value
+    const task = inputBox.value.trim()
 
-    todo.push(task)
+    if(task === ""){
+        alert("Please enter a task")
+        return
+    }
 
+    // store as an object instead of plain text, so we can track completed status
+    todo.push({ text: task, completed: false })
+
+    inputBox.value = ""
+
+    saveTodo()
     renderTodo()
 }
 
 function createTodo(item, index){
     const li = document.createElement("li")
-    li.innerText = item
-    
+
+    // checkbox to mark task as completed
+    const checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+    checkbox.checked = item.completed
+    checkbox.addEventListener("change", ()=>{
+        toggleComplete(index)
+    })
+
+    const span = document.createElement("span")
+    span.innerText = item.text
+    if(item.completed){
+        span.style.textDecoration = "line-through"
+    }
+
+    const editbtn = document.createElement("button")
+    editbtn.innerText = "Edit"
+    editbtn.addEventListener("click", ()=>{
+        editList(index)
+    })
+
     const deletebtn = document.createElement("button")
     deletebtn.innerText = "Delete"
-
-    li.appendChild(deletebtn)
-    
     deletebtn.addEventListener("click", ()=>{
         deleteList(index)
     })
+
+    li.appendChild(checkbox)
+    li.appendChild(span)
+    li.appendChild(editbtn)
+    li.appendChild(deletebtn)
 
     return li;
 }
@@ -32,11 +75,29 @@ function createTodo(item, index){
 function deleteList(index){
     todo.splice(index, 1)
 
+    saveTodo()
+    renderTodo()
+}
+
+function editList(index){
+    const updatedText = prompt("Edit your task:", todo[index].text)
+
+    if(updatedText !== null && updatedText.trim() !== ""){
+        todo[index].text = updatedText.trim()
+        saveTodo()
+        renderTodo()
+    }
+}
+
+function toggleComplete(index){
+    todo[index].completed = !todo[index].completed
+
+    saveTodo()
     renderTodo()
 }
 
 function renderTodo(){
-    list.innerText = ""
+    list.innerHTML = ""
     todo.forEach((item, index)=>{
         const li = createTodo(item, index)
 
@@ -46,3 +107,6 @@ function renderTodo(){
 
 
 addTask.addEventListener("click", addTodo)
+
+
+loadTodo()
